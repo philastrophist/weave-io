e31d0fdfe7ac5f7a48f2a6a3ca6e92be4326dc53
Shaun Read <shaun.c.read@gmail.com>
2021-01-13 10:02:07 +0000

    partial

--- weaveio/readquery/parse_tree.py
+++ weaveio/readquery/parse_tree.py
@@ -229,39 +229,74 @@ def print_nested_list(nested, tab=0):
         else:
             print('    '*tab, entry)
 
+
+def shared_hierarchy_branch(graph, branch):
+    branches = reduce(and_, [set(p.find_hierarchy_branches()) for p in branch.parents])
+    distances = [(b, nx.shortest_path_length(graph, b, branch)) for b in branches]
+    return max(distances, key=lambda x: x[1])[0]
+
+def parse(graph) -> List:
+    aligns = [i for i in graph.nodes if isinstance(i.action, Alignment)][::-1]
+    shared_aligns = defaultdict(list)
+    for align in aligns:
+        shared_aligns[shared_hierarchy_branch(graph, align)].append(align)
+    query = []
+    todo = list(nx.algorithms.topological_sort(graph))
+    while todo:
+        node = todo.pop(0)
+        if node in shared_aligns:
+            align_list = shared_aligns[node]
+            for align in align_list:
+                inputs = align.action.branches
+                inputs += (align.action.reference, )
+                subqueries = []
+                for input_node in inputs:
+                    before = list(nx.descendants(graph, node)) + [node]
+                    after = list(nx.ancestors(graph, input_node)) + [input_node]
+                    newgraph = nx.subgraph_view(graph, lambda n: n in before and n in after)
+                    subquery = parse(newgraph)
+                    subqueries.append(subquery)
+                done = list(set(flatten(subqueries)))
+                for d in done:
+                    if d in todo:
+                        del todo[todo.index(d)]
+                reference_subquery = subqueries.pop(-1)
+                subqueries += reference_subquery
+                query += subqueries
+        else:
+            query.append(node)
+    return query
+
+
 if __name__ == '__main__':
-    # reduced = reduce_stages([[ [[ ['entry', 'ob', 'target', 'collect1', 'any1'], ['entry', 'ob', 'run', 'collect2', 'any2'] ], 'align0', 'or1'], ['entry', 'ob', 'exposures', 'expmjd>1', 'collect3', 'any3'] ], 'align1', '...', 'end'])
-    # print(reduced)
-    # print_nested_list(reduced)
+    handler = BranchHandler()
+    ob = handler.begin('OB')
+    target = ob.traverse(TraversalPath('->', 'target'))
+    run = ob.traverse(TraversalPath('->', 'run'))
+    exposure = ob.traverse(TraversalPath('->', 'exposure'))
+    ob_targets = ob.collect([], [target])
+    any_ob_targets = ob_targets.operate('{any}')
+    ob_runs = ob.collect([], [run])
+    any_ob_runs = ob_runs.operate('{any}')
+    ob_exposures = ob.collect([], [exposure])
+    any_ob_exposures = ob_exposures.operate('{any}')
 
-    # handler = BranchHandler()
-    # ob = handler.begin('OB')
-    # target = ob.traverse(TraversalPath('->', 'target'))
-    # run = ob.traverse(TraversalPath('->', 'run'))
-    # exposure = ob.traverse(TraversalPath('->', 'exposure'))
-    # ob_targets = ob.collect([], [target])
-    # any_ob_targets = ob_targets.operate('{any}')
-    # ob_runs = ob.collect([], [run])
-    # any_ob_runs = ob_runs.operate('{any}')
-    # ob_exposures = ob.collect([], [exposure])
-    # any_ob_exposures = ob_exposures.operate('{any}')
+    align0 = any_ob_runs.align(any_ob_targets)
+    or1 = align0.operate('{or}')
+    align1 = or1.align(any_ob_exposures)
+    or2 = align1.operate('{or}')
+    final = or2.filter('')
+    final = final.results({final: [final.hierarchies[-1].get('obid')]})
+    graph = final.relevant_graph
+    plot(graph, '/opt/project/weaveio_example_querytree_test_branch.png')
+    subqueries = parse(graph)
+    print_nested_list(subqueries)
     #
-    # align0 = any_ob_runs.align(any_ob_targets)
-    # or1 = align0.operate('{or}')
-    # align1 = or1.align(any_ob_exposures)
-    # or2 = align1.operate('{or}')
-    # final = or2.filter('')
-    # final = final.results({final: [final.hierarchies[-1].get('obid')]})
-    #
-    #
-    # subqueries = parse_tree3(final)
-    # print_nested_list(reduce_stages(subqueries))
-
-    from tree_test_weaveio_example import red_spectra
-    # plot(red_spectra.relevant_graph, '/opt/project/weaveio_example_querytree_red_branch.png')
-    subqueries = parse_tree3(red_spectra.results({}))
-    print_nested_list(reduce_stages(subqueries))
-
-
-
+    # from tree_test_weaveio_example import red_spectra
+    # final = red_spectra.results({})
+    # graph = final.relevant_graph
+    # plot(graph, '/opt/project/weaveio_example_querytree_red_branch.png')
+    # plot(final.accessible_graph, '/opt/project/weaveio_example_querytree_red_branch_accessible.png')
+    # subqueries = parse(graph)
+    # print_nested_list(subqueries)
 
