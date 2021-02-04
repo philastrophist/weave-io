e95fcca988592fe6deadc9c66b45657f49c59ea9
Shaun C Read <shaun.c.read@gmail.com>
2021-02-04 12:29:44 +0000

    version bump

--- weaveio/__version__.py
+++ weaveio/__version__.py
@@ -1,2 +1,2 @@
-__version__ = "2021.1.5"
-__changes__ = 'Added arrows back to improve performance and added attr columns'
+__version__ = "2021.1.6"
+__changes__ = 'added skip. reverted attr columns...'
