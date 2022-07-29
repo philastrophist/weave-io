e8ccd2a77823dc76abf07a15344f87ec6f164756
Shaun C Read <shaun.read@nottingham.ac.uk>
2022-07-29 17:44:57 +0100

    problematic

--- new_test.py
+++ new_test.py
@@ -5,14 +5,16 @@ from weaveio.opr3 import Data
 from astropy.table import Table
 
 data = Data()
-print(data.redrocks[['cname', 'targra', 'targdec']](limit=50))
-sys.exit(0)
+# print(data.redrocks[['cname', 'targra', 'targdec']](limit=50))
+# sys.exit(0)
+explain(data.l1single_spectra)
 
-table = Table.read('weaveio/tests/my_table.ascii', format='ascii')
-rows, targets = join(table, 'cname', data.weave_targets)
-q = targets.cname
-mjds = targets.exposures.mjd
-q = targets['cname', rows['modelMag_i'], {'mjds': mjds, 'nobservations': count(mjds, wrt=targets)}]
+# table = Table.read('weaveio/tests/my_table.ascii', format='ascii')
+# rows, targets = join(table, 'cname', data.weave_targets)
+# print(mean(targets.l1single_spectra.snr, wrt=data)())
+# q = targets.cname
+# mjds = targets.exposures.mjd
+# q = targets['cname', rows['modelMag_i'], {'mjds': mjds, 'nobservations': count(mjds, wrt=targets)}]
 # exposures = q()
 # print(exposures)
 
@@ -42,11 +44,11 @@ q = targets['cname', rows['modelMag_i'], {'mjds': mjds, 'nobservations': count(m
 # targets = targets[any(targets.redrocks.zwarn == 0, wrt=targets)]
 # print(count(data.weave_targets[exists(data.weave_targets.redrocks, wrt=data.weave_targets)])())
 
-zs = targets.redrocks[targets.redrocks.zwarn == 0].z
-q = targets[['cname', {'redshifts': zs, 'mean_redshift': mean(zs, wrt=targets)}]]
-
-cypher, params = q._precompile()._to_cypher()
-print('\n'.join(cypher))
-t = q(limit=100)
-print(t)
+# zs = targets.redrocks[targets.redrocks.zwarn == 0].z
+# q = targets[['cname', {'redshifts': zs, 'mean_redshift': mean(zs, wrt=targets)}]]
+#
+# cypher, params = q._precompile()._to_cypher()
+# print('\n'.join(cypher))
+# t = q(limit=100)
+# print(t)
 # print(len(t), t.colnames)
