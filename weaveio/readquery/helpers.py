65891b59b124d0aba6b50ab1517d2bdc623464c2
Shaun C Read <shaun.read@nottingham.ac.uk>
2022-07-29 17:44:39 +0100

    added more helpers

--- weaveio/readquery/helpers.py
+++ weaveio/readquery/helpers.py
@@ -4,8 +4,22 @@ from .base import BaseQuery
 
 
 def attributes(query: BaseQuery) -> List[str]:
+    from ..data import Data
+    if isinstance(query._data, Data):
+        return list(query._data.singular_factors.keys())
     return query._data.class_hierarchies[query._obj].products_and_factors
+
+def objects(query: BaseQuery) -> List[str]:
+    from ..data import Data
+    if isinstance(query._data, Data):
+        return list(query._data.singular_hierarchies.keys())
+    h = query._data.class_hierarchies[query._obj]
+    neighbors = [i.singular_name for i in query._data.hierarchy_graph.neighbors(h)]
+    return neighbors
 
 def explain(query: BaseQuery) -> None:
     print(f"{query._obj}:")
     print(f"\t {query._data.class_hierarchies[query._obj].__doc__.strip()}")
+
+def find(query: BaseQuery, guess: str) -> List[str]:
+    return query._data.find_names(guess)
