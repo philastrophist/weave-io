edd002e720daca6a3e64026694e9df82d1aef17b
Shaun Read <shaun.c.read@gmail.com>
2022-08-08 17:20:51 +0100

    version bump

--- weaveio/__version__.py
+++ weaveio/__version__.py
@@ -1,2 +1,2 @@
-__version__ = "2022.1.4"
-__changes__ = 'Bugfixes and improvements: filtering no longer sometimes results in undiagnosable errors.'
+__version__ = "2022.1.5"
+__changes__ = 'console & installation: use `weaveio console` for interactive console and `weaveio upgrade` to handle updates'
